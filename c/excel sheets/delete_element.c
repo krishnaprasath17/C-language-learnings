54fa71e87297f0819f2c67c699ddb991583108ab
Your Name <you@example.com>
2025-12-06 10:37:43 +0530

    Delete elemnt from given psition and print all

---
+++ c/excel sheets/delete_element.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int n,pos;
+    scanf("%d",&n);
+
+    int a[n];
+    for(int i = 0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    scanf("%d",&pos);
+    for(int i = pos -1;i<n-1;i++){
+        a[i] = a[i+1];
+    }
+    n--;
+    for(int i = 0;i < n;i++){
+        printf("%d",a[i]);
+    }
+
+}
